70610b8dad628a7e70b1387821b22ff4aec6e6d6
Frokesy <akindeleayanfeoluwa@gmail.com>
2024-01-22 20:04:00 +0100

    designed doctors and departments components

--- app/components/defaults/Header.jsx
+++ app/components/defaults/Header.jsx
@@ -4,11 +4,11 @@ import Logo from "../../images/logo.png";
 
 const Header = () => {
   return (
-    <header className="flex justify-between items-center text-[#000] pt-6 px-16 bg-white">
-      <Image src={Logo} className="w-20" alt="img" />
+    <header className="flex justify-between items-center text-[#000] pt-6 px-16 pb-2 bg-white">
+      <Image src={Logo} className="w-[4rem]" alt="img" />
       <div className="space-x-16">
         <Link href="/" className="hover:text-[#3aaba9] hover:font-bold">Home</Link>
-        <Link href="/" className="hover:text-[#3aaba9] hover:font-bold">Find a Doctor</Link>
+        <Link href="/find-a-doctor" className="hover:text-[#3aaba9] hover:font-bold">Find a Doctor</Link>
         <Link href="/" className="hover:text-[#3aaba9] hover:font-bold">About Us</Link>
         <Link href="/" className="hover:text-[#3aaba9] hover:font-bold">Contact Us</Link>
       </div>
---
+++ app/find-a-doctor/page.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Header from "../components/defaults/Header";
+import Image from "next/image";
+import Woman from "../images/doctor-01.png";
+import Doctors from "../components/sections/DoctorSearch/Doctors";
+import Departments from "../components/sections/DoctorSearch/Departments";
+
+const DoctorSearch = () => {
+  return (
+    <div>
+      <Header />
+      <div className="bg-[#3aaba9] px-16 flex justify-between">
+        <div className="pt-[15vh]">
+          <h2 className="text-[73px] font-semibold capitalize">
+            Find available doctor for different ailment.
+          </h2>
+          <p className="text-[20px] pt-6">
+            Lorem ipsum dolor sit amet consectetur. Neque pellentesque tincidunt
+            diam bibendum amet sit nec lobortis. Ipsum dignissim.
+          </p>
+          <div className="input-field flex justify-between mt-6 bg-[#fff] w-[60%] rounded-lg px-4 py-2">
+            <input type="text" className="text-[#333] w-[100%] bg-transparent outline-none border-none" />
+            <button className="bg-[#3aaba9] py-1 px-6 rounded-lg">search</button>
+          </div>
+        </div>
+        <Image
+          src={Woman}
+          alt="doctor-img-01"
+          className="h-[800px] w-[800px]"
+        />
+      </div>
+
+      <Doctors />
+      <Departments />
+    </div>
+  );
+};
+
+export default DoctorSearch;
