575e92780cbf199b754e5abdd6c238760dbfed0d
Ayomide <vayomide2@gmail.com>
2024-01-26 15:41:59 +0100

    adjust import

--- app/book-appointment/[id]/page.jsx
+++ app/book-appointment/[id]/page.jsx
@@ -1,10 +1,10 @@
 import React, { useMemo } from "react";
 import Header from "../../components/defaults/Header";
 import Footer from "../../components/defaults/Footer";
-import Banner from "@/app/find-a-doctor/banner";
-import { doctors } from "@/app/_constants/doctors";
-import SelectedDoctor from "@/app/book-appointment/[id]/_components/selected-doctor";
-import AppointmentForm from "@/app/book-appointment/[id]/_components/appointment-form";
+import Banner from "../../find-a-doctor/banner";
+import { doctors } from "../../_constants/doctors";
+import SelectedDoctor from "../../book-appointment/[id]/_components/selected-doctor";
+import AppointmentForm from "../../book-appointment/[id]/_components/appointment-form";
 
 export default function BookAppointmentPage({ params }) {
   const doctor = useMemo(() => {
